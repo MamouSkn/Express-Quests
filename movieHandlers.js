77481a1515533ead09c105daea88c39fd03b6f0e
Mamou Skn <mamou.test@gmail.com>
2023-09-26 22:05:43 +0200

    Quête express 02 ok

--- movieHandlers.js
+++ movieHandlers.js
@@ -1,3 +1,5 @@
+const database = require("./database");
+
 const movies = [
   {
     id: 1,
@@ -32,13 +34,20 @@ const getMovies = (req, res) => {
 const getMovieById = (req, res) => {
   const id = parseInt(req.params.id);
 
-  const movie = movies.find((movie) => movie.id === id);
-
-  if (movie != null) {
-    res.json(movie);
-  } else {
-    res.status(404).send("Not Found");
-  }
+  // const movie = movies.find((movie) => movie.id === id);
+  database
+    .query("select * from movies where id = ?", [id])
+    .then(([movie]) => {
+      if (movie != null) {
+        res.json(movie[0]);
+        } else {
+          res.status(404).send("Not Found");
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error retrieving data from database");
+      });
 };
 
 module.exports = {
